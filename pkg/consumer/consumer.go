53b3729cd9eb27a3ba4ff477ae8cdb83416fd9a7
Alexander Timonin <2sdaiwxet@gmail.com>
2026-06-02 19:29:57 +0500

    add logging to kafka consumer

--- pkg/consumer/consumer.go
+++ pkg/consumer/consumer.go
@@ -3,7 +3,7 @@ package consumer
 import (
 	"context"
 	"encoding/json"
-	"log"
+	"log/slog"
 	"pkg/events"
 
 	"github.com/segmentio/kafka-go"
@@ -13,10 +13,11 @@ type HandlerFunc[T events.Event] func(ctx context.Context, event T) error
 
 type KafkaConsumer[T events.Event] struct {
 	reader  *kafka.Reader
+	log     *slog.Logger
 	handler HandlerFunc[T]
 }
 
-func NewKafkaConsumer[T events.Event](brokers []string, groupID string, handler HandlerFunc[T]) *KafkaConsumer[T] {
+func NewKafkaConsumer[T events.Event](brokers []string, groupID string, log *slog.Logger, handler HandlerFunc[T]) *KafkaConsumer[T] {
 	var zeroEvent T
 	topic := zeroEvent.GetTopic()
 
@@ -29,31 +30,33 @@ func NewKafkaConsumer[T events.Event](brokers []string, groupID string, handler 
 
 	return &KafkaConsumer[T]{
 		reader:  reader,
+		log:     log,
 		handler: handler,
 	}
 }
 
 func (c *KafkaConsumer[T]) Start(ctx context.Context) {
-	log.Printf("Консьюмер для топика [%s] запущен...", c.reader.Config().Topic)
+	log := c.log.With(slog.String("topic", c.reader.Config().Topic))
+	log.Info("consumer is running")
 
 	for {
 		msg, err := c.reader.FetchMessage(ctx)
 		if err != nil {
-			log.Printf("ошибка чтения сообщения из %s: %v", c.reader.Config().Topic, err)
+			log.Error("failed to read message", slog.Any("err", err))
 			return
 		}
 
 		var event T
 		if err := json.Unmarshal(msg.Value, &event); err != nil {
-			log.Printf("ошибка демаршалинга: %v", err)
+			log.Error("failed to unmarshal message", slog.Any("err", err))
 			c.reader.CommitMessages(ctx, msg)
 			continue
 		}
 
 		err = c.handler(ctx, event)
 		if err != nil {
-			log.Printf("ошибка обработки события в %s: %v", c.reader.Config().Topic, err)
-			// continue // возможно, стоит не коммитить, чтобы попытаться обработать позже
+			log.Error("failed to process message", slog.Any("err", err))
+			// continue // TODO: возможно, стоит не коммитить, чтобы попытаться обработать позже
 		}
 
 		c.reader.CommitMessages(ctx, msg)
