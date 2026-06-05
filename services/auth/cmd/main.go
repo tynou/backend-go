3803063cbb8c5e5e2d56c44e3dbd6ac919c1a8cf
Alexander Timonin <2sdaiwxet@gmail.com>
2026-06-05 11:27:47 +0500

    dockerize auth and setup Grafana logging stack

--- services/auth/cmd/main.go
+++ services/auth/cmd/main.go
@@ -59,5 +59,5 @@ func main() {
 }
 
 func setupLogger() *slog.Logger {
-	return slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	return slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
 }
